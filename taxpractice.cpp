28c8af253c9b284ac15c6ba2cf6dac68bad04700
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-08-24 13:22:06 +0200

    improvents to exercise

--- taxpractice.cpp
+++ taxpractice.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main() {
-    const int sales = 950000;
+    const double sales = 950000;
     const int state_tax_rate = 4;
     const int county_tax_rate = 2;
 
-    const int state_tax = sales / 100 * state_tax_rate;
-    const int county_tax = sales / 100 * county_tax_rate;
+    const double state_tax = sales / 100 * state_tax_rate;
+    const double county_tax = sales / 100 * county_tax_rate;
 
-    cout << "Sales: " << sales << endl
-         << "State Tax: " << state_tax << endl
-         << "County Tax: " << county_tax;
+    cout << "Sales: $" << sales << endl
+         << "State Tax: $" << state_tax << endl
+         << "County Tax: $" << county_tax;
     return 0;
 }
